c73efca589d7f976811eb228987114e3cb931226
tanerdundar <t.dundar145@mail.com>
2022-09-21 19:32:58 +0300

    1-23 arası

---
+++ javademos/conditionals/Main.java
@@ -0,0 +1,16 @@
+package javademos.conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 20;
+		if (sayi<20) {
+			System.out.println("Sayi 20den kucuktur.");
+		}else if (sayi==20) {
+			System.out.println("Sayi 20e esittir.");
+		}else {
+			System.out.println("Sayi 20den buyuktur.");
+		}
+	}
+
+}
---
+++ javademos/conditionals/switchDemo/Main.java
@@ -0,0 +1,31 @@
+package javademos.conditionals.switchDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		char grade = 'C';
+
+		switch (grade) {
+
+		case 'A':
+			System.out.println("Mukemmel : Gectiniz!");
+			break;
+		case 'B':
+		case 'C':
+			System.out.println("Iyi : Gectiniz!");
+			break;
+		case 'D':
+			System.out.println("Fena Degil : Gectiniz!");
+			break;
+		case 'F':
+			System.out.println("Maalesef Kaldiniz!");
+			break;
+
+		default:
+			System.out.println("Gecersiz not gidiniz");
+		}
+
+	}
+
+}
